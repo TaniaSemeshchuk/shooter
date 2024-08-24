99aa11ae03063f8b7394f9cf451d3b9b8d648b1c
Tania Semeshchuk <74670974+TaniaSemeshchuk@users.noreply.github.com>
2024-08-24 15:37:19 +0300

    Update shooter_game.py

--- shooter_game.py
+++ shooter_game.py
@@ -10,7 +10,13 @@ fire_sound = mixer.Sound('fire.ogg')
 
 img_back = "galaxy.jpg"
 img_hero = "rocket.png"
+img_bullet = "bullet.png"
 
+font.init()
+font1 = font.Font(None, 80)
+font2 = font.Font(None, 36)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
 
 lost = 0
 score = 0
@@ -45,7 +51,8 @@ class Player(GameSprite):
             self.rect.x += self.speed
     
     def fire(self):
-        pass
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
 
 class Enemy(GameSprite):
     def update(self):
@@ -56,6 +63,11 @@ class Enemy(GameSprite):
             self.rect.y = 0
             lost = lost + 1
 
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
     
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 monsters = sprite.Group()
@@ -64,19 +76,32 @@ for i in range(1, 6):
                     randint(1, 5))
     monsters.add(monster)
 
+bullets = sprite.Group()
+
 run = True
 finish = False
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
     if not finish:
-        window.blit(background, (0, 0))
+        window.blit(background, (0, 0)) 
+        text = font2.render('Рахунок: '+str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
+        text_lose = font2.render('Пропущено: '+str(lost), 1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
+
+
         ship.update()
         monsters.update()
-
+        bullets.update()
         ship.reset()
         monsters.draw(window)
+        bullets.draw(window)
 
     
     
